b75d63c6a2896e88aed76c164184fc40581ebc00
Jamile <milli.santana2017@gmail.com>
2023-07-15 15:55:05 -0300

    fix: Ajustes e outras correcoes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("./database/db");
+const userRoute = require("./routes/userRoutes")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 mongoose.connect();
+
+app.use("/user", userRoute);
 
 module.exports = app;
